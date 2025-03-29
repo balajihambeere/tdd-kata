1fa845e818c5d1f82575f9211c1289673f6f0e25
balajihambeere <balaji.hambere@gmail.com>
2025-03-29 15:05:43 +0530

    added comments on logic

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,4 +1,7 @@
+// StringCalculator.ts
 export function add(numbers: string): number {
+    // Empty string returns 0
     if (numbers === '') return 0;
+    // Single number returns the number itself
     return parseInt(numbers);
 }
