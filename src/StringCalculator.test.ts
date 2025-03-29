8b6626dc7950d36a8b5269b924188f4329b5930c
balajihambeere <balaji.hambere@gmail.com>
2025-03-29 14:57:44 +0530

    Written failing test for single number returns the value

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -4,4 +4,8 @@ describe('StringCalculator', () => {
     it('should return 0 for an empty string', () => {
         expect(add('')).toBe(0);
     });
+
+    it('should return the number itself when only one number is passed', () => {
+        expect(add('1')).toBe(1);
+    });
 });
