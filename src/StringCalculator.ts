4d44b9730e4374132607f516f4f18b833f2e5860
balajihambeere <balaji.hambere@gmail.com>
2025-03-29 15:28:17 +0530

    Refactored minimum code to pass the test should support custom delimiters

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -8,8 +8,26 @@ export function add(numbers: string): number {
       2. Two numbers, comma delimited, returns the sum
       3. handle any amount of numbers, comma delimited
       4. handle newlines between numbers
+      5. support different delimiters
      */
-    const nums = numbers.split(/[,\n]/) // regex to split by comma or newline
+    let delimiter = /[,\n]/; // Default delimiters are comma and newline
+
+    let numsString = numbers;
+    // Check for custom delimiter
+    // If the string starts with "//", it indicates a custom delimiter
+    if (numbers.startsWith('//')) {
+
+        const delimiterEndIndex = numbers.indexOf('\n'); // Find the index of the first newline character
+
+        const customDelimiter = numbers.substring(2, delimiterEndIndex); // Extract the custom delimiter
+        delimiter = new RegExp(`[${customDelimiter}\n]`); // Create a regex for the custom delimiter and newline
+
+        // Extract the numbers string after the custom delimiter declaration
+        // For example, if the input is "//;\n1;2", this will give "1;2"
+        numsString = numbers.substring(delimiterEndIndex + 1); // 
+    }
+
+    const nums = numsString.split(delimiter)
         .map(num => parseInt(num));
 
     return nums.reduce((sum, num) => sum + num, 0);
