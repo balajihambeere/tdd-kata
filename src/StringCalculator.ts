688d173deb7a99a4c99867851afc615faf0f2e5c
balajihambeere <balaji.hambere@gmail.com>
2025-03-29 15:08:50 +0530

    Refactored minimum code to pass the test should return the sum of two numbers separated by comma

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -3,5 +3,11 @@ export function add(numbers: string): number {
     // Empty string returns 0
     if (numbers === '') return 0;
     // Single number returns the number itself
-    return parseInt(numbers);
+    /*
+      1. Single number returns the value
+      2. Two numbers, comma delimited, returns the sum
+     */
+    return numbers.split(',')
+        .map(num => parseInt(num))
+        .reduce((sum, num) => sum + num, 0);
 }
