97314c9067eba0c72d0d210e2a49752cde36273d
balajihambeere <balaji.hambere@gmail.com>
2025-03-29 16:44:55 +0530

    Written failed test which should handle multiple delimiters

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -29,6 +29,11 @@ describe('StringCalculator', () => {
         expect(() => add('1,-2,3')).toThrow('negative numbers not allowed -2');
     });
     it('should show all negative numbers in exception message', () => {
-        expect(() => add('1,-2,-3')).toThrowError('negative numbers not allowed -2,-3');
+        expect(() => add('1,-2,-3')).toThrow('negative numbers not allowed -2,-3');
     });
+
+    it('should handle multiple delimiters', () => {
+        expect(add('//[*][%]\n1*2%3')).toBe(6);
+    });
+
 });
