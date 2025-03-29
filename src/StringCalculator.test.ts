dbae026a7590cc0852c7694facdda87665212dea
balajihambeere <balaji.hambere@gmail.com>
2025-03-29 15:20:29 +0530

    Written failed test which should support custom delimiters

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -20,4 +20,8 @@ describe('StringCalculator', () => {
     it('should handle new lines between numbers', () => {
         expect(add('1\n2,3')).toBe(6);
     });
+
+    it('should support custom delimiters', () => {
+        expect(add('//;\n1;2')).toBe(3);
+    });
 });
